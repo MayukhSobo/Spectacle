7b83ed8d601bc21eaf3ce241e1c6d94377a2d9d1
MayukhSobo <mayukh2012@hotmail.com>
2025-01-23 19:26:09 +0530

    Fixed the banner rendering issue

--- db/database.go
+++ db/database.go
@@ -1,8 +1,8 @@
 package db
 
 type Database interface {
-	Connect()
-	Ping()
-	Execute()
-	Close()
+	Get(key string) (string, error)
+	Set(key string, value string) error
+	Delete(key string) error
+	Close() error
 }
